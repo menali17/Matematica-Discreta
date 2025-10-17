aaa8623c171e1c714173225f0a28f4999f519afc
menali17 <enzomenali@gmail.com>
2025-10-17 13:07:40 -0300

    modified:   questao4.c

--- questao4.c
+++ questao4.c
@@ -99,3 +99,23 @@ int main() {
  O resultado final é 4
  
  */
+
+ /*
+ Questãon 4 - Perguntas V ou F
+ (V) O algoritmo de Euclides estendido é utilizado para calcular o inverso modular de um número.
+ (F) Se mdc(G, Zn) ≠ 1, o programa ainda consegue encontrar o inverso de G em Zn.
+ (V) A operação (H * inverso) % Zn representa a divisão modular de H por G.
+ (V) Se n1 for primo, o código aplica o Pequeno Teorema de Fermat para simplificar o cálculo de a^x mod n1.
+ (F) A função powMod implementa o cálculo de potência modular utilizando multiplicações diretas sem otimização.
+ (V) Quando o resultado do inverso é negativo, o código ajusta o valor somando o módulo m0.
+ (V) O cálculo de fi(n1) (função totiente de Euler) é utilizado apenas quando n1 não é primo.
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
